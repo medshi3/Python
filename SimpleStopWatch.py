59897ec9cb3c9298a5e271022aa902867fd2b3e3
Mr noob <32881062+medshi3@users.noreply.github.com>
2018-07-12 13:59:00 +0100

    for 2.x python users
    i just added the 2.x python version of the code XD

--- SimpleStopWatch.py
+++ SimpleStopWatch.py
@@ -14,3 +14,19 @@ while True:
         endtime = time.time()
         print('Total Time:', round(endtime - starttime, 2),'secs')
         break
+
+        
+        #python 2.x version
+        import time
+
+print "Press ENTER to begin, Press Ctrl + C to stop"
+while True:
+    try:
+        raw_input() # For ENTER. 
+        starttime = time.time()
+        print "Started"
+    except KeyboardInterrupt:
+        print "Stopped"
+        endtime = time.time()
+        print "Total Time:", round(endtime - starttime, 2), secs
+	break	
